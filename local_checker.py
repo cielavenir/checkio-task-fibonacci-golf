d927dc939bad066869a383e1d753c9023606463c
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-08-14 13:13:43 +1200

    clear indents

--- local_checker.py
+++ local_checker.py
@@ -8,28 +8,32 @@
 
 import marshal
 
-def check_code(codestring):
-	codestring = codestring.replace("\r\n","\n")
-	codestring = codestring.replace("\r","\n")
-	codestring = codestring.rstrip()
-	try:
-		return len( marshal.dumps(__builtins__.compile(codestring,'','exec',optimize=1)) )
-	except SyntaxError as detail:
-		import traceback
-		lines = traceback.format_exception_only(SyntaxError, detail)
-		for line in lines:
-			print(line.replace('File "<string>"',''))
-		return None
 
-if __name__=='__main__':
-	# cf: http://www.opensource.apple.com/source/python/python-3/python/Lib/py_compile.py
-	import sys
-	if len(sys.argv)<2:
-		print('local_checker py...')
-		sys.exit(1)
-	for i in range(1,len(sys.argv)):
-		f = open(sys.argv[i])
-		codestring = f.read()
-		f.close()
-		len = check_code(codestring)
-		print(len)
+def check_code(codestring):
+    codestring = codestring.replace("\r\n", "\n")
+    codestring = codestring.replace("\r", "\n")
+    codestring = codestring.rstrip()
+    try:
+        return len(marshal.dumps(__builtins__.compile(codestring, '', 'exec', optimize=1)))
+    except SyntaxError as detail:
+        import traceback
+
+        lines = traceback.format_exception_only(SyntaxError, detail)
+        for line in lines:
+            print(line.replace('File "<string>"', ''))
+        return None
+
+
+if __name__ == '__main__':
+    # cf: http://www.opensource.apple.com/source/python/python-3/python/Lib/py_compile.py
+    import sys
+
+    if len(sys.argv) < 2:
+        print('local_checker py...')
+        sys.exit(1)
+    for i in range(1, len(sys.argv)):
+        f = open(sys.argv[i])
+        codestring = f.read()
+        f.close()
+        len = check_code(codestring)
+        print(len)
--- verification/tests.py
+++ verification/tests.py
@@ -8,89 +8,90 @@ Each test is dict with
 """
 
 #This is very basic and verbose solution. You need to compress this code as much as possible (in compiled form).
-def fibgolf_basic(sequence,num):
-	if sequence=='fibonacci':
-		i=2
-		a=0
-		b=1
-		while True:
-			nxt=a+b
-			if i==num: return nxt
-			a=b
-			b=nxt
-			i+=1
-	if sequence=='tribonacci':
-		i=3
-		a=0
-		b=1
-		c=1
-		while True:
-			nxt=a+b+c
-			if i==num: return nxt
-			a=b
-			b=c
-			c=nxt
-			i+=1
-	if sequence=='lucas':
-		i=2
-		a=2
-		b=1
-		while True:
-			nxt=a+b
-			if i==num: return nxt
-			a=b
-			b=nxt
-			i+=1
-	if sequence=='jacobsthal':
-		i=2
-		a=0
-		b=1
-		while True:
-			nxt=2*a+b
-			if i==num: return nxt
-			a=b
-			b=nxt
-			i+=1
-	if sequence=='pell':
-		i=2
-		a=0
-		b=1
-		while True:
-			nxt=a+2*b
-			if i==num: return nxt
-			a=b
-			b=nxt
-			i+=1
-	if sequence=='perrin':
-		i=3
-		a=3
-		b=0
-		c=2
-		while True:
-			nxt=a+b
-			if i==num: return nxt
-			a=b
-			b=c
-			c=nxt
-			i+=1
-	if sequence=='padovan':
-		i=3
-		a=0
-		b=1
-		c=1
-		while True:
-			nxt=a+b
-			if i==num: return nxt
-			a=b
-			b=c
-			c=nxt
-			i+=1
+def fibgolf_basic(sequence, num):
+    if sequence == 'fibonacci':
+        i = 2
+        a = 0
+        b = 1
+        while True:
+            nxt = a + b
+            if i == num: return nxt
+            a = b
+            b = nxt
+            i += 1
+    if sequence == 'tribonacci':
+        i = 3
+        a = 0
+        b = 1
+        c = 1
+        while True:
+            nxt = a + b + c
+            if i == num: return nxt
+            a = b
+            b = c
+            c = nxt
+            i += 1
+    if sequence == 'lucas':
+        i = 2
+        a = 2
+        b = 1
+        while True:
+            nxt = a + b
+            if i == num: return nxt
+            a = b
+            b = nxt
+            i += 1
+    if sequence == 'jacobsthal':
+        i = 2
+        a = 0
+        b = 1
+        while True:
+            nxt = 2 * a + b
+            if i == num: return nxt
+            a = b
+            b = nxt
+            i += 1
+    if sequence == 'pell':
+        i = 2
+        a = 0
+        b = 1
+        while True:
+            nxt = a + 2 * b
+            if i == num: return nxt
+            a = b
+            b = nxt
+            i += 1
+    if sequence == 'perrin':
+        i = 3
+        a = 3
+        b = 0
+        c = 2
+        while True:
+            nxt = a + b
+            if i == num: return nxt
+            a = b
+            b = c
+            c = nxt
+            i += 1
+    if sequence == 'padovan':
+        i = 3
+        a = 0
+        b = 1
+        c = 1
+        while True:
+            nxt = a + b
+            if i == num: return nxt
+            a = b
+            b = c
+            c = nxt
+            i += 1
+
 
 TESTS = {}
 import random
 
-category=['fibonacci','tribonacci','lucas','jacobsthal','pell','perrin','padovan']
-for i,e in enumerate(category):
-	TESTS[str(i+1)+'. '+e]=[]
-	for n in random.sample(range(10,400),8):
-		TESTS[str(i+1)+'. '+e].append({"input":[e,n],"answer":fibgolf_basic(e,n)})
+category = ['fibonacci', 'tribonacci', 'lucas', 'jacobsthal', 'pell', 'perrin', 'padovan']
+for i, e in enumerate(category):
+    TESTS[str(i + 1) + '. ' + e] = []
+    for n in random.sample(range(10, 400), 8):
+        TESTS[str(i + 1) + '. ' + e].append({"input": [e, n], "answer": fibgolf_basic(e, n)})

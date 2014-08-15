0bf55b8d473782ba5c0a082ae2a16eb3e06debf2
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-08-15 13:57:09 +1200

    js

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -1,6 +1,6 @@
 //Dont change it
-requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
-    function (ext, $, TableComponent) {
+requirejs(['ext_editor_1', 'jquery_190', 'raphael_210', 'snap.svg_030'],
+    function (ext, $, Raphael, Snap) {
 
         var cur_slide = {};
 
@@ -8,7 +8,9 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
         });
 
         ext.set_process_in(function (this_e, data) {
+            cur_slide = {};
             cur_slide["in"] = data[0];
+            this_e.addAnimationSlide(cur_slide);
         });
 
         ext.set_process_out(function (this_e, data) {
@@ -17,8 +19,6 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
 
         ext.set_process_ext(function (this_e, data) {
             cur_slide.ext = data;
-            this_e.addAnimationSlide(cur_slide);
-            cur_slide = {};
         });
 
         ext.set_process_err(function (this_e, data) {
@@ -28,8 +28,47 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
         });
 
         ext.set_animate_success_slide(function (this_e, options) {
-            var $h = $(this_e.setHtmlSlide('<div class="animation-success"><div></div></div>'));
-            this_e.setAnimationHeight(115);
+            var ends = ["th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th"]
+
+            options = options || {};
+            var is_new_record = options.is_new_record || false;
+            var place_rating = String(options.place_rating || 0);
+            var best_points = options.best_points || 0;
+            var current_points = options.current_points || 0;
+            var $div = $("<div></div>");
+            var $h = $(this_e.setHtmlSlide('<div class="animation-success"><div class="result"></div></div>'));
+            var $resultDiv = $h.find(".result");
+            var $table = $("<table></table>").addClass("numbers");
+            if (is_new_record) {
+                $resultDiv.addClass("win-sign");
+                $resultDiv.append($("<div></div>").text("You beat your best results!"));
+                var $tr = $("<tr></tr>");
+                $tr.append($("<th></th>").text(best_points));
+                $tr.append($("<th></th>").text(place_rating).append($("<span></span>").addClass(".ends").text(ends[Number(place_rating[place_rating.length - 1])])));
+
+                $table.append($tr);
+                $tr = $("<tr></tr>");
+                $tr.append($("<td></td>").text("Personal best"));
+                $tr.append($("<td></td>").text("Place"));
+                $table.append($tr);
+            }
+            else {
+                $resultDiv.addClass("norm-sign");
+                $resultDiv.append($("<div></div>").text("Your results"));
+                $tr = $("<tr></tr>");
+                $tr.append($("<th></th>").text(current_points));
+                $tr.append($("<th></th>").text(best_points));
+                $tr.append($("<th></th>").text(place_rating).append($("<span></span>").addClass(".ends").text(ends[Number(place_rating[place_rating.length - 1])])));
+
+                $table.append($tr);
+                $tr = $("<tr></tr>");
+                $tr.append($("<td></td>").text("Points"));
+                $tr.append($("<td></td>").text("Personal best"));
+                $tr.append($("<td></td>").text("Place"));
+                $table.append($tr);
+            }
+            $resultDiv.append($table);
+            this_e.setAnimationHeight(255);
         });
 
         ext.set_animate_slide(function (this_e, data, options) {
@@ -43,10 +82,10 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
             var fname = 'fibgolf';
 
             var checkioInput = data.in;
-            var checkioInputStr = fname + '(' + JSON.stringify(checkioInput[0]) + ",<br>" + JSON.stringify(checkioInput[1]) + ')';
+            var checkioInputStr = fname + '(' + JSON.stringify(checkioInput[0]) + ", " + JSON.stringify(checkioInput[1]) + '"])';
 
-            var failError = function(dError) {
-                $content.find('.call').html('Fail: ' + checkioInputStr);
+            var failError = function (dError) {
+                $content.find('.call').html(checkioInputStr);
                 $content.find('.output').html(dError.replace(/\n/g, ","));
 
                 $content.find('.output').addClass('error');
@@ -66,28 +105,33 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
                 return false;
             }
 
-            var rightResult = data.ext["answer"];
-            var userResult = data.out;
-            var result = data.ext["result"];
-            var result_addon = data.ext["result_addon"];
+            $content.find('.call').html(checkioInputStr);
+            $content.find('.output').html('Working...');
 
+            if (data.ext) {
+                var rightResult = data.ext["answer"];
+                var userResult = data.out;
+                var result = data.ext["result"];
+                var result_addon = data.ext["result_addon"];
 
-            //if you need additional info from tests (if exists)
-            var explanation = data.ext["explanation"];
+                //if you need additional info from tests (if exists)
+                var explanation = data.ext["explanation"];
+                $content.find('.output').html('&nbsp;Your result:&nbsp;' + JSON.stringify(userResult));
+                if (!result) {
+                    $content.find('.answer').html('Right result:&nbsp;' + JSON.stringify(rightResult));
+                    $content.find('.answer').addClass('error');
+                    $content.find('.output').addClass('error');
+                    $content.find('.call').addClass('error');
+                }
+                else {
+                    $content.find('.answer').remove();
+                }
 
-            $content.find('.output').html('&nbsp;Your result:&nbsp;' + JSON.stringify(userResult));
-
-            if (!result) {
-                $content.find('.call').html('Fail: ' + checkioInputStr);
-                $content.find('.answer').html('Right result:&nbsp;' + JSON.stringify(rightResult));
-                $content.find('.answer').addClass('error');
-                $content.find('.output').addClass('error');
-                $content.find('.call').addClass('error');
             }
             else {
-                $content.find('.call').html('Pass: ' + checkioInputStr);
                 $content.find('.answer').remove();
             }
+
 
             //Your code here about test explanation animation
             //$content.find(".explanation").html("Something text for example");
@@ -115,6 +159,7 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
 //                this_e.sendToConsoleCheckiO("something");
 //            });
 //        });
+
 
         var colorOrange4 = "#F0801A";
         var colorOrange3 = "#FA8F00";

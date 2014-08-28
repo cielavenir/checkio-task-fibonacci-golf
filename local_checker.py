3617064b4e3845cdd194a36d6c6a4d2c0453dbc4
cielavenir <cielartisan@gmail.com>
2014-08-29 01:42:55 +0900

    add Ruby local_checker (for some sake?)

--- local_checker.py
+++ local_checker.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 # Print "compiled size".
-# They say Ruby can use YARVCore::InstructionSequence, but I cannot find any examples.
-# Note: if MAJOR or MINOR version changes, rejudge is required (TEENY does not matter).
+# Note: if MAJOR or MINOR version (libpython.so version) changes,
+# rejudge is required (TEENY does not matter).
 
 # Due to optimize=1 flag, Python 3.2+ is required.
+# (Local default is optimize=0 and CheckiO server's seems to be optimize=1)
 
 import marshal
-
 
 def check_code(codestring):
     codestring = codestring.replace("\r\n", "\n")
@@ -17,17 +17,14 @@ def check_code(codestring):
         return len(marshal.dumps(__builtins__.compile(codestring, '', 'exec', optimize=1)))
     except SyntaxError as detail:
         import traceback
-
         lines = traceback.format_exception_only(SyntaxError, detail)
         for line in lines:
             print(line.replace('File "<string>"', ''))
         return None
 
-
 if __name__ == '__main__':
     # cf: http://www.opensource.apple.com/source/python/python-3/python/Lib/py_compile.py
     import sys
-
     if len(sys.argv) < 2:
         print('local_checker py...')
         sys.exit(1)
